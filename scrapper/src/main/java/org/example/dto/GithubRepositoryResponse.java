fd694916a68ea56c1337017635b85463db111df5
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-04-15 12:24:20 +0300

    add new functions in GitHub and Stackoverflow clients and theirs new responses

--- scrapper/src/main/java/org/example/dto/GithubRepositoryResponse.java
+++ scrapper/src/main/java/org/example/dto/GithubRepositoryResponse.java
@@ -6,6 +6,22 @@ import java.sql.Timestamp;
 
 public record GithubRepositoryResponse(
 ) {
+    public record CommitBody(
+            Commit commit
+    ){
+        public record Commit(
+                Author author,
+                String message
+        ){
+            public record Author(
+                    String name,
+                    Timestamp date
+            ){}
+        }
+    }
+    public record Branch(
+            String name
+    ){}
     public record Event(
             @JsonProperty("created_at")
             Timestamp createdAt
