13c63d533bc9327593450dde4f68a560cb29d2a1
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-04-15 12:24:20 +0300

    add new functions in GitHub and Stackoverflow clients and theirs new responses

--- scrapper/src/main/java/org/example/service/StackoverflowClient.java
+++ scrapper/src/main/java/org/example/service/StackoverflowClient.java
@@ -1,12 +1,15 @@
 package org.example.service;
 
+import org.example.dto.StackoverflowAnswerResponse;
 import org.example.dto.StackoverflowQuestionsResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
-@HttpExchange(url = "/2.3/questions/{id}?site=stackoverflow")
+@HttpExchange(url = "/2.3/questions/{id}")
 public interface StackoverflowClient {
-    @GetExchange
+    @GetExchange("?site=stackoverflow")
     StackoverflowQuestionsResponse getQuestion(@PathVariable String id);
+    @GetExchange("answers?site=stackoverflow")
+    StackoverflowAnswerResponse getAnswer(@PathVariable String id);
 }
